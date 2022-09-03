65a602a9eb3619cf93aebdd825f2e36dd93828db
jaehan.kim <mju6013@naver.com>
2022-09-04 04:47:43 +0900

    PostForm 모듈화

--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -2,22 +2,14 @@
 	<div>
 		<h2>게시글 수정</h2>
 		<hr class="my-4" />
-		<form @submit.prevent>
-			<div class="mb-3">
-				<label for="title" class="form-label">제목</label>
-				<input v-model="form.title" type="text" class="form-control" id="title" />
-			</div>
-			<div class="mb-3">
-				<label for="content" class="form-label">내용</label>
-				<textarea v-model="form.content"  class="form-control" id="content" rows="3"></textarea>
-			</div>
-			<div class="pt-4">
-				<button type="button" class="btn btn-outline-danger me-2" @click="goDetailPage">
-					취소
-				</button>
-				<button class="btn btn-primary" @click="edit">수정</button>
-			</div>
-		</form>
+		<PostForm v-model:title="form.title" v-model:content="form.content" @submit.prevent>
+			<template #actions>
+					<button type="button" class="btn btn-outline-danger " @click="goDetailPage">
+						취소
+					</button>
+					<button class="btn btn-primary" @click="edit">수정</button>
+			</template>
+		</PostForm>
 	</div>
 </template>
 
@@ -25,6 +17,7 @@
 import { ref } from '@vue/reactivity';
 import { useRoute, useRouter } from 'vue-router';
 import { getPostById, updatePost } from '@/api/posts'
+import PostForm from '@/components/posts/PostForm.vue';
 
 const route = useRoute();
 const router = useRouter();
