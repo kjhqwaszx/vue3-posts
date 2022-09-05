c84b6ec6c26226dee9ced0fd821960fe702f6978
jaehan.kim <mju6013@naver.com>
2022-09-05 21:11:33 +0900

    custom directive 적용

--- src/main.js
+++ src/main.js
@@ -7,12 +7,19 @@ import funcPlugins from '@/plugins/func';
 import objPlugins from '@/plugins/obj';
 import person from '@/plugins/person';
 import globalComponents from '@/plugins/global-components';
+import focus from '@/directives/focus';
 
 const app = createApp(App);
-app.use(funcPlugins);
-app.use(objPlugins, { name: 'Kyobo' });
+// app.use(funcPlugins);
+// app.use(objPlugins, { name: 'Kyobo' });
+
+//global 프로퍼티(변수) 등록
 app.use(person, { name: '교보생명' });
+
+//global 컴포넌트 등록
 app.use(globalComponents);
+// global 디렉티브 등록
+app.directive('focus', focus);
 
 app.use(router).mount('#app');
 
